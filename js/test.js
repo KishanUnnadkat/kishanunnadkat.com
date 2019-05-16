14acb055bdfee256eb2324258113bcdf9cb3e690
Kish Unnadkat <kdu@shadbolt.local>
2019-05-16 14:01:53 +0100

    Improved styling and added googlemaps api

---
+++ js/test.js
@@ -0,0 +1,5 @@
+function initMap() {
+    var uluru = { lat: 52.62973, lng: - 1.13897  };
+    var map = new google.maps.Map(document.getElementById('map'), { zoom: 15, center: uluru });
+    var marker = new google.maps.Marker({ position: uluru, map: map });
+}
